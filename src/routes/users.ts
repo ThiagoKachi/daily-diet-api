eecdce1a97ee92fc5f3561969212115f30f6b7e1
Thiago Kachinsky <thiagokachidev@gmail.com>
2024-02-13 16:17:35 -0300

    feat: first commit

---
+++ src/routes/users.ts
@@ -0,0 +1,66 @@
+/* eslint-disable camelcase */
+import { FastifyInstance } from 'fastify'
+import { randomUUID } from 'node:crypto'
+import { ZodError, z } from 'zod'
+import { knex } from '../database'
+
+interface CreateUserBody {
+  success: boolean
+  data: {
+    name: string
+    email: string
+  }
+  error: ZodError
+}
+
+export async function usersRoutes(app: FastifyInstance) {
+  app.get('/', async (request) => {
+    const { sessionId } = request.cookies
+    const users = await knex('users').where('session_id', sessionId).select('*')
+
+    return users
+  })
+
+  app.post('/', async (request, reply) => {
+    const createUserBodySchema = z.object({
+      name: z
+        .string({
+          required_error: 'Name is required',
+          invalid_type_error: 'Name must be a string',
+        })
+        .min(1),
+      email: z
+        .string({
+          required_error: 'E-mail is required',
+          invalid_type_error: 'E-mail must be a string',
+        })
+        .email(),
+    })
+
+    const user = createUserBodySchema.safeParse(request.body) as CreateUserBody
+
+    if (!user.success) {
+      throw new Error(user.error.issues[0].message)
+    }
+
+    let sessionId = request.cookies.sessionId
+
+    if (!sessionId) {
+      sessionId = randomUUID()
+
+      reply.cookie('sessionId', sessionId, {
+        path: '/',
+        maxAge: 60 * 60 * 24 * 7, // 7 days
+      })
+    }
+
+    await knex('users').insert({
+      id: randomUUID(),
+      name: user.data.name,
+      email: user.data.email,
+      session_id: sessionId,
+    })
+
+    return reply.status(201).send()
+  })
+}
---
+++ src/server.ts
@@ -0,0 +1,17 @@
+import fastifyCookie from '@fastify/cookie'
+import fastify from 'fastify'
+import { usersRoutes } from './routes/users'
+
+const app = fastify()
+app.register(usersRoutes, {
+  prefix: '/users',
+})
+app.register(fastifyCookie)
+
+app
+  .listen({
+    port: 3333,
+  })
+  .then(() => {
+    console.log('Server is running on port 3333')
+  })
