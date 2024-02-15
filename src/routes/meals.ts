7e165a58ba43b74dce44b3247eb517e878d002be
Thiago Kachinsky <thiagokachidev@gmail.com>
2024-02-15 14:13:24 -0300

    feat: implement PUT route and adjust routes returns

--- src/routes/meals.ts
+++ src/routes/meals.ts
@@ -9,6 +9,7 @@ interface createMealsBody {
     name: string
     description: string
     is_on_diet: boolean
+    date: Date
   }
   success: boolean
   error: ZodError
@@ -20,14 +21,17 @@ export async function mealsRoutes(app: FastifyInstance) {
     {
       preHandler: [checkSessionIdExists],
     },
-    async (request) => {
+    async (request, reply) => {
       const { sessionId } = request.cookies
 
-      const [{ id }] = await knex('users').where('session_id', sessionId)
+      const { id } = await knex('users').where('session_id', sessionId).first()
 
-      const response = await knex('meals').where('user_id', id).select('*')
+      const meals = await knex('meals')
+        .where('user_id', id)
+        .select('*')
+        .orderBy('date', 'desc')
 
-      return response
+      return reply.send(meals)
     },
   )
 
@@ -36,25 +40,20 @@ export async function mealsRoutes(app: FastifyInstance) {
     {
       preHandler: [checkSessionIdExists],
     },
-    async (request) => {
+    async (request, reply) => {
       const createMealParamSchema = z.object({
         id: z.string(),
       })
 
-      const { sessionId } = request.cookies
-
-      const [{ id }] = await knex('users').where('session_id', sessionId)
-
       const { id: mealId } = createMealParamSchema.parse(request.params)
 
-      const response = await knex('meals')
-        .where({
-          user_id: id,
-          id: mealId,
-        })
-        .select('*')
+      const meal = await knex('meals').where({ id: mealId }).first()
 
-      return response[0]
+      if (!meal) {
+        return reply.status(404).send({ error: 'Meal not found' })
+      }
+
+      return meal
     },
   )
 
@@ -63,25 +62,22 @@ export async function mealsRoutes(app: FastifyInstance) {
     {
       preHandler: [checkSessionIdExists],
     },
-    async (request) => {
+    async (request, reply) => {
       const createMealParamSchema = z.object({
         id: z.string(),
       })
 
-      const { sessionId } = request.cookies
-
-      const [{ id }] = await knex('users').where('session_id', sessionId)
-
       const { id: mealId } = createMealParamSchema.parse(request.params)
 
-      const response = await knex('meals')
-        .where({
-          user_id: id,
-          id: mealId,
-        })
-        .delete('*')
+      const meal = await knex('meals').where({ id: mealId }).first()
 
-      return response[0]
+      if (!meal) {
+        return reply.status(404).send({ error: 'Meal not found' })
+      }
+
+      await knex('meals').where({ id: mealId }).delete()
+
+      return reply.status(204).send()
     },
   )
 
@@ -130,6 +126,59 @@ export async function mealsRoutes(app: FastifyInstance) {
       })
 
       return reply.status(201).send()
+    },
+  )
+
+  app.put(
+    '/:id',
+    {
+      preHandler: [checkSessionIdExists],
+    },
+    async (request, reply) => {
+      const editeMealParamSchema = z.object({
+        id: z.string(),
+      })
+
+      const { id: mealId } = editeMealParamSchema.parse(request.params)
+
+      const updateMealBodySchema = z.object({
+        name: z
+          .string({
+            invalid_type_error: 'Name must be a string',
+          })
+          .min(1),
+        description: z.string({
+          invalid_type_error: 'Description must be a string',
+        }),
+        is_on_diet: z.boolean({
+          invalid_type_error: 'Is on diet must be a boolean',
+        }),
+        date: z.coerce.date(),
+      })
+
+      const meal = updateMealBodySchema.safeParse(
+        request.body,
+      ) as createMealsBody
+
+      if (!meal.success) {
+        throw new Error(meal.error.issues[0].message)
+      }
+
+      const mealToEdit = await knex('meals').where('id', mealId).first()
+
+      if (!mealToEdit) {
+        return reply.status(404).send({ error: 'Meal not found' })
+      }
+
+      await knex('meals').where({ id: mealId }).update({
+        name: meal.data.name,
+        description: meal.data.description,
+        is_on_diet: meal.data.is_on_diet,
+        date: meal.data.date.toISOString(),
+        updated_at: new Date().toISOString(),
+      })
+
+      return reply.status(204).send()
     },
   )
 }
